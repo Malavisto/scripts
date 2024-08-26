0fdf4a4319b64858a94e34a751844a016ad7f73b
Malavisto <malavistobusiness@gmail.com>
2024-08-26 22:38:41 +0200

    Modified Scripts

--- send-notif.sh
+++ send-notif.sh
@@ -1,16 +1,5 @@
-#!/bin/bash
-
 #Collect Variables
 source ./config.env
-
-# Specify the hard drive you want to test
-DRIVE="/dev/sda"  # Replace /dev/sdX with your actual drive (e.g., /dev/sda)
-
-# Run a short SMART test
-smartctl -t short "$DRIVE"
-
-# Wait for the test to complete
-sleep 300  # 5 minutes; adjust based on the test type (short or long)
 
 # Log the results
 LOGFILE="/var/log/smart_test_result.log"
